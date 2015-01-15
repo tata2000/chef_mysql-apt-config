edebaa8fcdf8bcf3a97e087331ca6242e8be378f
Tatyana Arenburg <tata@wix.com>
2015-01-15 14:31:40 +0200

    fixing versions of platforms

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures mysql-apt-config'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends           'dpkg_package'
-supports          'ubundu', '>=12'
-supports          'debian', '>=7'
+supports          'ubundu', '>=12.04'
+supports          'debian', '>=7.0'
