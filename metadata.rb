97280134f43fff3d67ee7ed7dc34b79ed71432a4
Tatyana Arenburg <tatyanak@users.noreply.github.com>
2015-01-25 11:38:39 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tatyanak@wix.com'
 license          'All rights reserved'
 description      'Installs/Configures mysql-apt-config'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
 depends           'dpkg_packages'
 supports          'ubundu', '>=12.04'
 supports          'debian', '>=7.0'
