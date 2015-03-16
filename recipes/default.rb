5985ac99fcf26116d8cdc7c429a53cf953eae112
Pavel Forkert <fxposter@gmail.com>
2015-03-16 17:33:51 +0200

    Fix typos

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,23 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
-file_name = node['mysql-apt-confing']['download_url'].split('/').last
+file_name = node['mysql-apt-config']['download_url'].split('/').last
 
 local_file_name="#{Chef::Config[:file_cache_path]}/#{file_name}"
 
 remote_file local_file_name do
-  source node['mysql-apt-confing']['download_url']
+  source node['mysql-apt-config']['download_url']
 end.run_action(:create_if_missing)
 
-md5 = Digest::MD5.file(local_file_name).hexdigest 
+md5 = Digest::MD5.file(local_file_name).hexdigest
 
-if node['mysql-apt-confing']['download_md5']==md5
+if node['mysql-apt-config']['download_md5']==md5
   dpkg_package local_file_name do
     action :nothing
     notifies :run, 'execute[apt-get update]', :immediately
   end.run_action(:install)
 else
-  raise "MD5 does not match. The MD5 of downloaded file is #{md5} and not #{node['mysql-apt-confing']['download_md5']}"
+  raise "MD5 does not match. The MD5 of downloaded file is #{md5} and not #{node['mysql-apt-config']['download_md5']}"
 end
 
 
